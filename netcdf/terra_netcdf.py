8c64f60bdfce0958e4fd3e4ca0dc0a224d1d25bc
Max Burnette <mburnet88@gmail.com>
2017-05-24 15:02:30 -0500

    Update for terrautils

--- netcdf/terra_netcdf.py
+++ netcdf/terra_netcdf.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python
-import logging
-import json
-import os, shutil
-import subprocess
 
 import datetime
-from dateutil.parser import parse
-from influxdb import InfluxDBClient, SeriesHelper
+import logging
+import json
+import os
+import subprocess
 
 from pyclowder.extractors import Extractor
 from pyclowder.utils import CheckMessage
 import pyclowder.files
 import pyclowder.datasets
+import terrautils.extractors
 
 
 class NetCDFMetadataConversion(Extractor):
 	def __init__(self):
 		Extractor.__init__(self)
 
+		influx_host = os.getenv("INFLUXDB_HOST", "terra-logging.ncsa.illinois.edu")
+		influx_port = os.getenv("INFLUXDB_PORT", 8086)
+		influx_db = os.getenv("INFLUXDB_DB", "extractor_db")
+		influx_user = os.getenv("INFLUXDB_USER", "terra")
+		influx_pass = os.getenv("INFLUXDB_PASSWORD", "")
+
 		# add any additional arguments to parser
-		# self.parser.add_argument('--max', '-m', type=int, nargs='?', default=-1,
-		#                          help='maximum number (default=-1)')
 		self.parser.add_argument('--output', '-o', dest="output_dir", type=str, nargs='?',
 								 default="/home/extractor/sites/ua-mac/Level_1/netcdf",
 								 help="root directory where timestamp & output directories will be created")
 		self.parser.add_argument('--overwrite', dest="force_overwrite", type=bool, nargs='?', default=False,
 								 help="whether to overwrite output file if it already exists in output directory")
 		self.parser.add_argument('--influxHost', dest="influx_host", type=str, nargs='?',
-								 default="terra-logging.ncsa.illinois.edu", help="InfluxDB URL for logging")
+								 default=influx_host, help="InfluxDB URL for logging")
 		self.parser.add_argument('--influxPort', dest="influx_port", type=int, nargs='?',
-								 default=8086, help="InfluxDB port")
+								 default=influx_port, help="InfluxDB port")
 		self.parser.add_argument('--influxUser', dest="influx_user", type=str, nargs='?',
-								 default="terra", help="InfluxDB username")
+								 default=influx_user, help="InfluxDB username")
 		self.parser.add_argument('--influxPass', dest="influx_pass", type=str, nargs='?',
-								 default="", help="InfluxDB password")
+								 default=influx_pass, help="InfluxDB password")
 		self.parser.add_argument('--influxDB', dest="influx_db", type=str, nargs='?',
-								 default="extractor_db", help="InfluxDB databast")
+								 default=influx_db, help="InfluxDB database")
 
 		# parse command line and load default logging configuration
 		self.setup()
@@ -47,27 +50,26 @@ class NetCDFMetadataConversion(Extractor):
 		# assign other arguments
 		self.output_dir = self.args.output_dir
 		self.force_overwrite = self.args.force_overwrite
-		self.influx_host = self.args.influx_host
-		self.influx_port = self.args.influx_port
-		self.influx_user = self.args.influx_user
-		self.influx_pass = self.args.influx_pass
-		self.influx_db = self.args.influx_db
+		self.influx_params = {
+			"host": self.args.influx_host,
+			"port": self.args.influx_port,
+			"db": self.args.influx_db,
+			"user": self.args.influx_user,
+			"pass": self.args.influx_pass
+		}
 
 	# Check whether dataset already has output files
 	def check_message(self, connector, host, secret_key, resource, parameters):
 		ds_md = pyclowder.datasets.get_info(connector, host, secret_key, resource['parent']['id'])
-		outPathRoot = self.getOutputFilename(ds_md['name'])
+		out_dir = terrautils.extractors.get_output_directory(self.output_dir, ds_md['name'], True)
 
-		foundAll = True
-		for outpath in ['.cdl', 'xml', '.json']:
-			if not os.path.isfile(os.path.join(outPathRoot, resource['name'].replace(".nc", outpath))):
-				foundAll = False
+		for out_type in ['.cdl', 'xml', '.json']:
+			out_fname = terrautils.extractors.get_output_filename(ds_md['name'], out_type)
+			if not os.path.isfile(os.path.join(out_dir, out_fname)):
+				return CheckMessage.download
 
-		if not foundAll:
-			return CheckMessage.download
-		else:
-			logger.info("skipping %s; outputs already exist" % resource['id'])
-			return CheckMessage.ignore
+		logging.info("skipping %s; outputs already exist" % resource['id'])
+		return CheckMessage.ignore
 
 	# Process the file and upload the results
 	def process_message(self, connector, host, secret_key, resource, parameters):
@@ -77,75 +79,49 @@ class NetCDFMetadataConversion(Extractor):
 
 		# Determine output file path
 		ds_md = pyclowder.datasets.get_info(connector, host, secret_key, resource['parent']['id'])
-		outPath = self.getOutputFilename(ds_md['name'])
-		if not os.path.isdir(outPath):
-			os.makedirs(outPath)
+		out_dir = terrautils.extractors.get_output_directory(self.output_dir, ds_md['name'], True)
+		if not os.path.isdir(out_dir):
+			os.makedirs(out_dir)
+		out_fname_root = terrautils.extractors.get_output_filename(ds_md['name'], '')
 
-		inPath = resource['local_paths'][0]
-		fileRoot = resource['name'].replace(".nc", "")
 
-		metaFilePath = os.path.join(outPath, fileRoot+'.cdl')
+		metaFilePath = os.path.join(out_dir, out_fname_root+'.cdl')
 		if not os.path.isfile(metaFilePath) or self.force_overwrite:
 			logging.info('...extracting metadata in cdl format: %s' % metaFilePath)
 			with open(metaFilePath, 'w') as fmeta:
-				subprocess.call(['ncks', '--cdl', '-m', '-M', inPath], stdout=fmeta)
+				subprocess.call(['ncks', '--cdl', '-m', '-M', resource['local_paths'][0]], stdout=fmeta)
 			created += 1
 			bytes += os.path.getsize(metaFilePath)
 			pyclowder.files.upload_to_dataset(connector, host, secret_key, resource['parent']['id'], metaFilePath)
 
-		metaFilePath = os.path.join(outPath, fileRoot+'.xml')
+		metaFilePath = os.path.join(out_dir, out_fname_root+'.xml')
 		if not os.path.isfile(metaFilePath) or self.force_overwrite:
 			logging.info('...extracting metadata in xml format: %s' % metaFilePath)
 			with open(metaFilePath, 'w') as fmeta:
-				subprocess.call(['ncks', '--xml', '-m', '-M', inPath], stdout=fmeta)
+				subprocess.call(['ncks', '--xml', '-m', '-M', resource['local_paths'][0]], stdout=fmeta)
 			created += 1
 			bytes += os.path.getsize(metaFilePath)
 			pyclowder.files.upload_to_dataset(connector, host, secret_key, resource['parent']['id'], metaFilePath)
 
-		metaFilePath = os.path.join(outPath, fileRoot+'.json')
+		metaFilePath = os.path.join(out_dir, out_fname_root+'.json')
 		if not os.path.isfile(metaFilePath) or self.force_overwrite:
 			logging.info('...extracting metadata in json format: %s' % metaFilePath)
 			with open(metaFilePath, 'w') as fmeta:
-				subprocess.call(['ncks', '--jsn', '-m', '-M', inPath], stdout=fmeta)
+				subprocess.call(['ncks', '--jsn', '-m', '-M', resource['local_paths'][0]], stdout=fmeta)
 			created += 1
 			bytes += os.path.getsize(metaFilePath)
 			pyclowder.files.upload_to_dataset(connector, host, secret_key, resource['parent']['id'], metaFilePath)
 
 			# Add json metadata to original netCDF file
 			with open(metaFilePath, 'r') as metajson:
-				jdata = {
-					# TODO: Generate JSON-LD context for additional fields
-					"@context": ["https://clowder.ncsa.illinois.edu/contexts/metadata.jsonld"],
-					"file_id": resource['id'],
-					"content": json.load(metajson),
-					"agent": {
-						"@type": "cat:extractor",
-						"extractor_id": host + "/api/extractors/" + self.extractor_info['name']
-					}
-				}
-				pyclowder.files.upload_metadata(connector, host, secret_key, resource['id'], jdata)
+				metadata = terrautils.extractors.build_metadata(host, self.extractor_info['name'], resource['id'],
+																json.load(metajson), 'dataset')
+				pyclowder.files.upload_metadata(connector, host, secret_key, resource['parent']['id'], metadata)
+
 
 		endtime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-		self.logToInfluxDB(starttime, endtime, created, bytes)
-
-	def getOutputFilename(self, ds_name):
-		# Determine output file path
-		if ds_name.find(" - ") > -1:
-			# sensor - timestamp
-			ds_name_parts = ds_name.split(" - ")
-			sensor_name = ds_name_parts[0]
-			if ds_name_parts[1].find("__") > -1:
-				# sensor - date__time
-				ds_time_parts = ds_name_parts[1].split("__")
-				timestamp = os.path.join(ds_time_parts[0], ds_name_parts[1])
-			else:
-				timestamp = ds_name_parts[1]
-			# /sensor/date/time
-			subPath = os.path.join(sensor_name, timestamp)
-		else:
-			subPath = ds_name
-
-		return os.path.join(self.output_dir, subPath)
+		terrautils.extractors.log_to_influxdb(self.extractor_info['name'], self.influx_params,
+											  starttime, endtime, created, bytes)
 
 if __name__ == "__main__":
 	extractor = NetCDFMetadataConversion()
--- sensorposition/terra_sensorposition.py
+++ sensorposition/terra_sensorposition.py
@@ -17,19 +17,25 @@ class Sensorposition2Geostreams(Extractor):
 	def __init__(self):
 		Extractor.__init__(self)
 
+		influx_host = os.getenv("INFLUXDB_HOST", "terra-logging.ncsa.illinois.edu")
+		influx_port = os.getenv("INFLUXDB_PORT", 8086)
+		influx_db = os.getenv("INFLUXDB_DB", "extractor_db")
+		influx_user = os.getenv("INFLUXDB_USER", "terra")
+		influx_pass = os.getenv("INFLUXDB_PASSWORD", "")
+
 		self.parser.add_argument('--plots', dest="plots_shp", type=str, nargs='?',
 								 default="/home/extractor/extractors-metadata/sensorposition/shp/sorghumexpfall2016v5/sorghumexpfall2016v5_lblentry_1to7.shp",
 								 help=".shp file containing plots")
 		self.parser.add_argument('--influxHost', dest="influx_host", type=str, nargs='?',
-								 default="terra-logging.ncsa.illinois.edu", help="InfluxDB URL for logging")
+								 default=influx_host, help="InfluxDB URL for logging")
 		self.parser.add_argument('--influxPort', dest="influx_port", type=int, nargs='?',
-								 default=8086, help="InfluxDB port")
+								 default=influx_port, help="InfluxDB port")
 		self.parser.add_argument('--influxUser', dest="influx_user", type=str, nargs='?',
-								 default="terra", help="InfluxDB username")
+								 default=influx_user, help="InfluxDB username")
 		self.parser.add_argument('--influxPass', dest="influx_pass", type=str, nargs='?',
-								 default="", help="InfluxDB password")
+								 default=influx_pass, help="InfluxDB password")
 		self.parser.add_argument('--influxDB', dest="influx_db", type=str, nargs='?',
-								 default="extractor_db", help="InfluxDB databast")
+								 default=influx_db, help="InfluxDB database")
 
 		# parse command line and load default logging configuration
 		self.setup()
@@ -39,11 +45,13 @@ class Sensorposition2Geostreams(Extractor):
 		logging.getLogger('__main__').setLevel(logging.DEBUG)
 
 		self.plots_shp = self.args.plots_shp
-		self.influx_host = self.args.influx_host
-		self.influx_port = self.args.influx_port
-		self.influx_user = self.args.influx_user
-		self.influx_pass = self.args.influx_pass
-		self.influx_db = self.args.influx_db
+		self.influx_params = {
+			"host": self.args.influx_host,
+			"port": self.args.influx_port,
+			"db": self.args.influx_db,
+			"user": self.args.influx_user,
+			"pass": self.args.influx_pass
+		}
 
 	# Check whether dataset has geospatial metadata
 	def check_message(self, connector, host, secret_key, resource, parameters):
@@ -145,7 +153,8 @@ class Sensorposition2Geostreams(Extractor):
 			terrautils.extractors.build_metadata(host, self.extractor_info['name'], resource['id'], metadata, 'dataset'))
 
 		endtime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-		terrautils.extractors.log_to_influxdb(self.extractor_info['name'], starttime, endtime, created, bytes)
+		terrautils.extractors.log_to_influxdb(self.extractor_info['name'], self.influx_params,
+											  starttime, endtime, created, bytes)
 
 
 if __name__ == "__main__":
