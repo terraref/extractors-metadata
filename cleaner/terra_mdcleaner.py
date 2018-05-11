99ecd7b570f7b72985f401800ea527135904e8b2
Max Burnette <mburnet2@illinois.com>
2018-05-11 10:44:56 -0500

    clean up logging, add some "skip" cases

--- cleaner/terra_mdcleaner.py
+++ cleaner/terra_mdcleaner.py
@@ -4,9 +4,9 @@ import os
 import logging
 
 from pyclowder.utils import CheckMessage
-from pyclowder.datasets import upload_metadata, submit_extraction
+from pyclowder.datasets import upload_metadata, submit_extraction, download_metadata
 from terrautils.extractors import TerrarefExtractor, delete_dataset_metadata, load_json_file
-from terrautils.metadata import clean_metadata
+from terrautils.metadata import clean_metadata, get_terraref_metadata
 
 
 def add_local_arguments(parser):
@@ -38,40 +38,51 @@ class ReCleanLemnatecMetadata(TerrarefExtractor):
 
 	# Process the file and upload the results
 	def process_message(self, connector, host, secret_key, resource, parameters):
-		self.start_message()
+		self.start_message(resource)
 
 		sensor_type, timestamp = resource['name'].split(" - ")
 
-		ignore_types = ["RGB GeoTIFFs", "Thermal IR GeoTIFFs"]
-		for ignore in ignore_types:
-			if sensor_type.find(ignore) > -1:
-				# We don't have json files for Level_1 data if they were queued here accidentally
-				return
+		# First, re-check metadata to verify it hasn't been added in meantime
+		ds_md = download_metadata(connector, host, secret_key, resource['id'])
+		terra_md = get_terraref_metadata(ds_md)
+		if terra_md:
+			self.log_info(resource, "Found TERRA-REF metadata; not cleaning")
+			return
+
+		# These datasets do not have TERRA md
+		uncleanables = ["Full Field"]
+		if sensor_type in uncleanables:
+			self.log_info(resource, "Cannot clean metadata for %s" % sensor_type)
+			return
+
+		# For these datasets, we must get TERRA md from raw_data source
+		lv1_types = {"RGB GeoTIFFs": "stereoTop",
+					 "Thermal IR GeoTIFFs": "flirIrCamera"}
+		if sensor_type in lv1_types:
+			raw_equiv = resource['name'].replace(sensor_type, lv1_types[sensor_type])
+			source_dir = os.path.dirname(self.sensors.get_sensor_path_by_dataset(raw_equiv))
+		else:
+			# Search for metadata.json source file
+			source_dir = os.path.dirname(self.sensors.get_sensor_path_by_dataset(resource['name']))
+		source_dir = self.remapMountPath(connector, source_dir)
 
 		if self.delete:
 			# Delete all existing metadata from this dataset
-			logging.getLogger(__name__).info("Deleting existing metadata from %s" % resource['id'])
+			self.log_info(resource, "Deleting existing metadata")
 			delete_dataset_metadata(host, self.clowder_user, self.clowder_pass, resource['id'])
 
-		# Search for metadata.json source file
-		source_dir = os.path.dirname(self.sensors.get_sensor_path_by_dataset(resource['name']))
-		source_dir = self.remapMountPath(connector, source_dir)
-
-		# TODO: Eventually we should find better way to represent this
-		# TODO: split between the PLY files (in Level_1) and metadata.json files
+		# TODO: split between the PLY files (in Level_1) and metadata.json files - unique to this sensor
 		if sensor_type == "scanner3DTop":
 			source_dir = source_dir.replace("Level_1", "raw_data")
 
-		logging.getLogger(__name__).info("Searching for metadata.json in %s" % source_dir)
-
+		self.log_info(resource, "Searching for metadata.json in %s" % source_dir)
 		if os.path.isdir(source_dir):
 			md_file = None
 			for f in os.listdir(source_dir):
 				if f.endswith("metadata.json"):
 					md_file = os.path.join(source_dir, f)
-
 			if md_file:
-				logging.getLogger(__name__).info("Found metadata.json; cleaning")
+				self.log_info("Found metadata.json; cleaning")
 				md_json = clean_metadata(load_json_file(md_file), sensor_type)
 				format_md = {
 					"@context": ["https://clowder.ncsa.illinois.edu/contexts/metadata.jsonld",
@@ -82,30 +93,30 @@ class ReCleanLemnatecMetadata(TerrarefExtractor):
 						"user_id": "https://terraref.ncsa.illinois.edu/clowder/api/users/%s" % self.userid
 					}
 				}
-				logging.getLogger(__name__).info("Uploading cleaned metadata to %s" % resource['id'])
+				self.log_info(resource, "Uploading cleaned metadata")
 				upload_metadata(connector, host, secret_key, resource['id'], format_md)
 
 				# Now trigger a callback extraction if given
 				if len(self.callback) > 0:
-					logging.getLogger(__name__).info("Submitting callback extraction to %s" % self.callback)
+					self.log_info(resource, "Submitting callback extraction to %s" % self.callback)
 					submit_extraction(connector, host, secret_key, resource['id'], self.callback)
 				else:
 					callbacks = self.get_callbacks_by_sensor(sensor_type)
 					if callbacks:
 						for c in callbacks:
-							logging.getLogger(__name__).info("Submitting callback extraction to %s" % c)
+							self.log_info(resource, "Submitting callback extraction to %s" % c)
 							submit_extraction(connector, host, secret_key, resource['id'], c)
 					else:
-						logging.getLogger(__name__).info("No default callback found for %s" % sensor_type)
+						self.log_info(resource, "No default callback found for %s" % sensor_type)
 			else:
-				logging.getLogger(__name__).error("metadata.json not found in %s" % source_dir)
+				self.log_error(resource, "metadata.json not found in %s" % source_dir)
 
 		else:
-			logging.getLogger(__name__).info("%s could not be found" % source_dir)
+			self.log_error(resource, "%s could not be found" % source_dir)
 
 		# TODO: Have extractor check for existence of Level_1 output product and delete if exists?
 
-		self.end_message()
+		self.end_message(resource)
 
 	def remapMountPath(self, connector, path):
 		if len(connector.mounted_paths) > 0:
